b64da5b54c5924b3a5ce98e2778a894b5f3b1e25
yuekesoft <1961339@qq.com>
2022-10-11 22:11:23 +0800

    edit nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,15 @@
-
 <template>
+    <i class="el-icon-menu"></i>
+
   <el-menu
     :default-active="activeIndex"
     class="el-menu-demo"
     mode="horizontal"
+    :ellipsis="false"
     @select="handleSelect"
   >
+    <el-menu-item index="0">LOGO</el-menu-item>
+    <div class="flex-grow" />
     <el-menu-item index="1">Processing Center</el-menu-item>
     <el-sub-menu index="2">
       <template #title>Workspace</template>
@@ -19,19 +23,20 @@
         <el-menu-item index="2-4-3">item three</el-menu-item>
       </el-sub-menu>
     </el-sub-menu>
-    <el-menu-item index="3">Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
   </el-menu>
-  <div class="h-6" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
 const activeIndex = ref('1')
-const activeIndex2 = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 </script>
 
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import ElementPlus from 'element-plus' // 引入element-plus
 import 'element-plus/theme-chalk/index.css' // 引入element-plus的样式
 
 
-import './assets/main.css' //默认页面的css样式
+//import './assets/main.css' //默认页面的css样式
 
 createApp(App)
 .use(ElementPlus, { zIndex: 3000, size: 'small' }) // 使用element-plus
